15c4604c5112616b3ef5b109cbd6cc3bc1927a05
Mike Boers <westernx@mikeboers.com>
2016-02-11 18:53:40 -0800

    Very basic test of ordering

---
+++ tests/test_order.py
@@ -0,0 +1,23 @@
+from . import *
+
+class TestOrder(ApiTestCase):
+
+    def test_basics(self):
+
+        sg = self.cached
+
+        shot = sg.create('Shot', {'code': 'multi_entity_test'})
+        for i in xrange(4):
+            sg.create('Task', {'entity': shot, 'content': '%d' % i})
+
+        tasks = sg.find('Task', [('entity', 'is', shot)], ['content'], order=[
+            {'field_name': 'id', 'direction': 'asc'}
+        ])
+        content = [e['content'] for e in tasks]
+        self.assertEqual(content, ['0', '1', '2', '3'])
+
+        tasks = sg.find('Task', [('entity', 'is', shot)], ['content'], order=[
+            {'field_name': 'id', 'direction': 'desc'}
+        ])
+        content = [e['content'] for e in tasks]
+        self.assertEqual(content, ['3', '2', '1', '0'])
