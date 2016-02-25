c2731d22adbf2abc29d73f5759d5d9f0fa124f5f
Mike Boers <westernx@mikeboers.com>
2016-02-25 10:56:54 -0800

    Add entity link to basic crud tests

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -3,19 +3,35 @@ from . import uuid
 
 def task_crud(self, shotgun, trigger_poll=lambda: None):
 
+    shot_name = uuid(8)
+    shot = shotgun.create('Shot', {'code': shot_name})
+
     name = uuid(8)
-    a = shotgun.create('Task', {'content': name})
+    task = shotgun.create('Task', {'content': name, 'entity': shot})
     trigger_poll()
-    b = self.cached.find_one('Task', [('id', 'is', a['id'])], ['content'])
-    self.assertSameEntity(a, b)
 
+    x = self.cached.find_one('Task', [('id', 'is', task['id'])], ['content'])
+    self.assertSameEntity(task, x)
+
+    # entity field
+    x = self.cached.find_one('Task', [('id', 'is', task['id'])], ['entity'])
+    self.assertSameEntity(shot, x['entity'])
+
+    # return through entity field
+    x = self.cached.find_one('Task', [('id', 'is', task['id'])], ['entity.Shot.code'])
+    self.assertEqual(shot_name, x['entity.Shot.code'])
+
+    # Updates
     name += '-2'
-    shotgun.update('Task', a['id'], {'content': name})
+    shotgun.update('Task', task['id'], {'content': name})
     trigger_poll()
-    c = self.cached.find_one('Task', [('id', 'is', a['id'])], ['content'])
-    self.assertEqual(c['content'], name)
+    x = self.cached.find_one('Task', [('id', 'is', task['id'])], ['content'])
+    self.assertEqual(x['content'], name)
 
-    shotgun.delete('Task', a['id'])
+    # Delete
+    shotgun.delete('Task', task['id'])
     trigger_poll()
-    d = self.cached.find_one('Task', [('id', 'is', a['id'])], ['content'])
-    self.assertIs(d, None)
+    x = self.cached.find_one('Task', [('id', 'is', task['id'])], ['content'])
+    self.assertIs(x, None)
+    x = self.cached.find_one('Task', [('id', 'is', task['id'])], ['content'], retired_only=True)
+    self.assertSameEntity(task, x)
